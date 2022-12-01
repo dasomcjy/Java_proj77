b8a57152aeca4a865ae9807e646db13781c807d3
Jiyoon Choi <dasomcjy@naver.com>
2022-12-01 18:26:29 +0900

    12/1 실습 커밋

---
+++ src/practice/ex01/P_11_29_02.java
@@ -0,0 +1,26 @@
+package practice.ex01;
+
+public class P_11_29_02 {
+	
+	
+	    public static String num(int i) {
+	        if(i==0){
+	            return "zero";
+	        } else if(i==1){
+	            return "one";
+	        } else if(i==2){
+	            return "two";
+	        }
+	        return "none";
+	    }
+	 
+	    public static void main(String[] args) {
+	        System.out.println(num(2));
+	    }
+	}
+
+	
+	    
+	
+
+
