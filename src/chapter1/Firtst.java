a7029b0237dede37920749902466b00952c1aec6
Jiyoon Choi <dasomcjy@naver.com>
2022-12-07 14:29:51 +0900

    12/7

---
+++ src/chapter1/Firtst.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Firtst {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
---
+++ src/chapter2/Valiable.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class Valiable {
+
+	public static void main(String[] args) {
+		int level;
+		level = 10;
+		System.out.println(level);
+
+		int age;
+		age = 36;
+		System.out.println(age);
+	}
+
+}
